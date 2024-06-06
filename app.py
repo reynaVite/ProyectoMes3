3bd87f2b8172d46eaa88cfe68be87b3ebfd510e4
Jesus Antonio Ramirez <tonchirmz@gmail.com>
2024-06-05 22:02:01 -0600

    Volviendo a lo de antes bro

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Jesus Antonio Ramirez Hernandez \n 20210700 \n 9 B "
+    return render_template('home.html', nombre='Mundo')
 
 if __name__ =='__main__':
     app.run(debug=True)
